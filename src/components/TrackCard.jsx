85a897d6a5d771b38911005354edecac67183ee2
zephyrus21 <pandey.piyus@gmail.com>
2023-01-14 18:32:32 +0530

    added views

---
+++ src/components/TrackCard.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const TrackCard = ({ track, view }) => {
+  const lineLength = 26;
+  const style =
+    view === "list"
+      ? "flex flex-col justify-start gap-4 text-white bg-stone-700 p-5 drop-shadow-md rounded-lg hover:scale-105 duration-200 cursor-pointer"
+      : "flex flex-row-reverse justify-end gap-4 items-center text-white bg-stone-700 p-3 drop-shadow-md rounded-lg hover:scale-[1.01] duration-200 cursor-pointer";
+
+  return (
+    <div className={style}>
+      <div>
+        <div>
+          {track.name.length > lineLength && view === "list"
+            ? track.name.substring(lineLength, 0).concat("...")
+            : track.name}
+        </div>
+        <div>
+          {track.album.name.length > lineLength && view === "list"
+            ? track.album.name.substring(lineLength, 0).concat("...")
+            : track.album.name}
+        </div>
+      </div>
+      <img
+        src={track.album.images[0].url}
+        alt='album_image'
+        className={`min-h-fit object-cover rounded-md ${
+          view === "grid" && "h-16"
+        }`}
+      />
+    </div>
+  );
+};
+
+export default TrackCard;
--- src/pages/Spoify.jsx
+++ src/pages/Spoify.jsx
@@ -7,10 +7,14 @@ import {
   setQuery,
 } from "../features/songs/songsSlice";
 import { useDispatch, useSelector } from "react-redux";
+import TrackCard from "../components/TrackCard";
 
 const Spoify = () => {
   const dispatch = useDispatch();
+
+  const [view, setView] = useState("grid");
   const [searchQuery, setSearchQuery] = useState("");
+  const [popularity, setPopularity] = useState("none");
 
   const tracks = useSelector(selectTracks);
   const status = useSelector(getTracksStatus);
@@ -24,34 +28,92 @@ const Spoify = () => {
   }, [dispatch, status]);
 
   useEffect(() => {
-    if (query) {
+    if (query !== "") {
       dispatch(fetchTracks());
     }
   }, [dispatch, query]);
 
   const onChangeHandler = (e) => {
     const { value } = e.target;
-    setSearchQuery(value);
-
-    dispatch(setQuery(value));
+    if (value === "") {
+      setSearchQuery("");
+      dispatch(setQuery("rock"));
+    } else {
+      setSearchQuery(value);
+      dispatch(setQuery(value));
+    }
   };
 
   if (status === "loading") {
     content = <p>Loading...</p>;
   } else if (status === "succeeded") {
-    const newTracks = tracks.filter((track) => track.popularity > 0);
+    const newTracks = tracks.filter((track) => {
+      if (popularity === "none") {
+        return track;
+      } else if (popularity === "low") {
+        return track.popularity < 30;
+      } else if (popularity === "med") {
+        return track.popularity > 30 && track.popularity < 70;
+      } else if (popularity === "high") {
+        return track.popularity > 70;
+      }
+    });
     content = newTracks.map((track) => (
-      <div className='bg-green-300'>{track.name}</div>
+      <TrackCard key={track.id} track={track} view={view} />
     ));
   } else if (status === "failed") {
+    content = <p>Failed to load tracks</p>;
   }
 
-  console.log(tracks);
+  const onOptionChangeHandler = (event) => {
+    setPopularity(event.target.value);
+  };
+
+  const onClickChangeView = () => {
+    if (view === "grid") {
+      setView("list");
+    } else {
+      setView("grid");
+    }
+  };
 
   return (
-    <div className='grid grid-cols-4 gap-6'>
-      <input type='text' value={searchQuery} onChange={onChangeHandler} />
-      {content}
+    <div className='p-5 justify-start align-start'>
+      <div className='flex justify-between bg-stone-600 p-6 rounded-lg'>
+        <input
+          className='border drop-shadow-2xl rounded-lg border-slate-900 pl-5 w-72'
+          type='text'
+          value={searchQuery}
+          onChange={onChangeHandler}
+          placeholder='Search for tracks and artists...'
+        />
+        <div className='flex items-center gap-4'>
+          <label htmlFor='popularity' className='text-lg text-white'>
+            Popularity
+          </label>
+          <select
+            name='popularity'
+            onChange={onOptionChangeHandler}
+            className='rounded-lg p-2 px-4'>
+            <option value='none'>None</option>
+            <option value='low'>Low</option>
+            <option value='med'>Medium</option>
+            <option value='high'>High</option>
+          </select>
+          <button
+            className='bg-stone-500 text-white rounded-lg p-2 px-4'
+            onClick={onClickChangeView}>
+            {view} view
+          </button>
+        </div>
+      </div>
+      {view === "list" ? (
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 grid-cols-1 gap-6 py-6'>
+          {content}
+        </div>
+      ) : (
+        <div className='flex flex-col gap-6 py-6'>{content}</div>
+      )}
     </div>
   );
 };
